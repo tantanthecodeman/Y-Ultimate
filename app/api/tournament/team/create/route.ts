cd1e64e3aa790886c60e4fd79e81e30ecb3b2f3d
Tanay Gujarathi <tanaygujarathig@gmail.com>
2025-10-31 23:53:12 +0530

    fix error for catch block

--- app/api/tournament/team/create/route.ts
+++ app/api/tournament/team/create/route.ts
@@ -17,8 +17,8 @@ export async function POST(request: Request){
 
     if (error) return NextResponse.json({ error: error.message }, { status: 500 });
     return NextResponse.json({ team: data });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-
-  }
+  } catch (err: unknown) {
+  const message = err instanceof Error ? err.message : 'An unexpected error occurred';
+  return NextResponse.json({ error: message }, { status: 500 });
+}
 }
